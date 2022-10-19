093b1c7031b8eca95b6f91ebdc7e75252533c729
Crowmorian <crowmorian@gmail.com>
2022-10-19 09:28:31 +0200

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/models.py
+++ testerToolkit_app/project/models.py
@@ -9,3 +9,10 @@ Created on Thu Oct 13 14:27:22 2022
 
 # Importing of necessary libraries
 #************************************
+from __init__ import db
+
+class User(db.model):
+    id = db.Column(db.integer, primary_key=True) #User ID serving as a primary key for identification
+    login = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
