719013f1d6f83da00de15dfa76d1d5f3209632ad
Crowmorian <crowmorian@gmail.com>
2022-10-19 14:28:09 +0200

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/auth.py
+++ testerToolkit_app/project/auth.py
@@ -8,7 +8,10 @@ Created on Thu Oct 13 14:17:00 2022
 
 # Importing of necessary libraries
 #************************************
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import User
+from . import db
 
 #Declaring routes and variables
 auth = Blueprint("auth", __name__)
@@ -24,3 +27,21 @@ def logout():
 @auth.route("/createUser")
 def createUser():
     return render_template('createUser.html')
+
+@auth.route('/createUser', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    
+    user = User.query.filter_by(username=username).first()
+    
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        return redirect(url_for('auth.createUser'))
+    
+    new_user = User(username=username, name=name, password=generate_password_hash(password, method='sha256'))
+    
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return redirect(url_for('main.index'))
