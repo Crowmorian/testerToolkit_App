b184dfee24930f140fe7ac6f0924d8b33984edde
Crowmorian <crowmorian@gmail.com>
2022-10-20 13:11:52 +0200

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/auth.py
+++ testerToolkit_app/project/auth.py
@@ -12,7 +12,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import Users
 from __init__ import db
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 
 #Declaring routes and variables
 auth = Blueprint("auth", __name__)
@@ -37,8 +37,10 @@ def login_post():
     return redirect(url_for('main.index'))
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "Logout"
+    logout_user()
+    return redirect(url_for('main.index'))
 
 @auth.route("/createUser")
 def createUser():
