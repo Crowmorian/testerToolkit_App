bee84493c2cf6b651ff2cb8259f580dc3200bcfd
Crowmorian <crowmorian@gmail.com>
2022-10-26 08:55:48 +0200

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/configFile.py
+++ testerToolkit_app/project/configFile.py
@@ -4,6 +4,16 @@ Created on Mon Oct 24 14:28:44 2022
 
 @author: Crowmorian
 """
+from flask import Blueprint, render_template
+
 #Declaring global variables and settings
-#global selectedLanguage
 selectedLanguage = "ENG"
+configFile = Blueprint("configFile", __name__)
+
+@configFile.route("/switchEng")
+def setEng ():
+    selectedLanguage = "ENG"
+
+@configFile.route("/switchCs")
+def setCS ():
+    selectedLanguage = "CS"
