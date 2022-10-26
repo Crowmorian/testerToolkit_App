91bea16f4e24960c2045a9756e5f04078f4f93f6
Crowmorian <crowmorian@gmail.com>
2022-10-26 10:07:37 +0200

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/main.py
+++ testerToolkit_app/project/main.py
@@ -19,7 +19,7 @@ main = Blueprint("main", __name__)
 def index():
     return render_template('index.html')
 
-@main.route("/")
+@main.route("cs/")
 def CSindex():
     return render_template('cs/index.html')
 
