1d3fcfa290b490df6ab9571775ae5aa542150eee
Crowmorian <crowmorian@gmail.com>
2022-10-27 09:16:14 +0200

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/configFile.py
+++ testerToolkit_app/project/configFile.py
@@ -4,32 +4,3 @@ Created on Mon Oct 24 14:28:44 2022
 
 @author: Crowmorian
 """
-
-
-# generate random integer values
-import random
-from random import randint
-from random import seed
-
-# seed random number generator
-seed(randint(10000,99999))
-
-howMany = 5
-whichNums = [0,1,2,3,4,5,6,7,8,9]
-canStartZero = False
-
-# generate some integers
-def randomNumber(howMany, whichNums, canStartZero):
-    randomSequence = (random.choices(whichNums, k = howMany))
-
-    if randomSequence[0] == 0:
-        randomNumber(howMany, whichNums, canStartZero)
-        print("Retrying")
-    else:
-        print(randomSequence)
-        
-randomNumber(howMany, whichNums, canStartZero)
-
-
-
-
---
+++ testerToolkit_app/project/tempTesting.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 14:28:44 2022
+
+@author: Crowmorian
+"""
+
+
+# generate random integer values
+from random import randint, seed, choices
+
+
+# seed random number generator
+
+
+howManyDigits = 5
+howManyNumbers = 6
+whichNums = [0,1,2,3,4,5,6,7,8,9]
+canStartZero = True
+results = []
+listToStr = ""
+
+
+# generate some integers
+def randomNumber(howManyDigits, whichNums, canStartZero, howManyNumbers):
+    seed(randint(10000,99999))
+    
+    for i in range(0,howManyNumbers):    
+        randomSequence = (choices(whichNums, k = howManyDigits))
+    
+        while randomSequence[0] == 0 and canStartZero == False:
+            randomSequence = (choices(whichNums, k = howManyDigits))
+        else:
+            listToStr = ' '.join([str(elem) for elem in randomSequence])
+            results.append(listToStr)
+            
+    print(results)
+        
+randomNumber(howManyDigits, whichNums, canStartZero, howManyNumbers)
