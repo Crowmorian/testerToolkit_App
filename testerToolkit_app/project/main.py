a452d2b8639465553f3f2cb09848ed5308dc3e9d
Crowmorian <crowmorian@gmail.com>
2022-11-02 11:05:38 +0100

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/main.py
+++ testerToolkit_app/project/main.py
@@ -227,6 +227,7 @@ def generateRandom_post():
         else:
             listToStr = ' '.join([str(elem) for elem in randomSequence])
             results.append(listToStr)
+    print(results, flush=True)
 
     return render_template('generateRandom.html',
         howManyDigits = session["howManyDigits"],
