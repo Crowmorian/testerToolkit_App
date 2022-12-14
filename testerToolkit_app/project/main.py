bd6ac6c9e6091ec7fc042381f623030524e53637
Crowmorian <crowmorian@gmail.com>
2022-12-14 11:34:00 +0100

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/main.py
+++ testerToolkit_app/project/main.py
@@ -1966,7 +1966,7 @@ def legalDates(start, end):
     randomDays = random.randrange(0, delta.days)
     
     subtraction = d2 - timedelta(days=randomDays)
-    randomDate = subtraction.strftime(('%d.%m. %Y'))        
+    randomDate = subtraction.strftime('%d.%m. %Y')        
         
     return(randomDate)
 
