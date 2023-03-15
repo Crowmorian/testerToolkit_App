ac36c05894c269ff2c6814777afa12fbaa339a0e
Crowmorian <crowmorian@gmail.com>
2023-03-15 14:48:51 +0100

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

---
+++ testerToolkit_app/project/templates/static/base.js
@@ -0,0 +1,6 @@
+const togglebutton = document.getElementByClassName("toggle-button")[0]
+const navbaLinks = document.getElementByClassName("navbar-links")[0]
+
+togglebutton.addEventListener("click". () => {
+    navbarLinks.classList("active")
+})
