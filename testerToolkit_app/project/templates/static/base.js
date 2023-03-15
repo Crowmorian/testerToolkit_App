8d6e981bce52c684959d02dfbfffed40bc72184f
Crowmorian <crowmorian@gmail.com>
2023-03-15 14:53:08 +0100

    Signed-off-by: Crowmorian <crowmorian@gmail.com>

--- testerToolkit_app/project/templates/static/base.js
+++ testerToolkit_app/project/templates/static/base.js
@@ -1,5 +1,5 @@
-const togglebutton = document.getElementByClassName("toggle-button")
-const navbarLinks = document.getElementByClassName("navbar-links")
+const togglebutton = document.getElementsByClassName("toggle-button")[0]
+const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 
 togglebutton.addEventListener("click", () => {
     navbarLinks.classList("active")
